5d07d7102cadcf8d854c9939d8d7bd27ddedf1f1
MayanthaS <sapumalmayantha@gmail.com>
2025-10-08 15:18:19 +0530

    learning the let and var

--- script.js
+++ script.js
@@ -5,10 +5,49 @@
 //var is function scope|let & const are block scope
 
 //--var---
+/*
 var num = 20;
 function multiply(){
     var num1 = 10;
     console.log(num);
     console.log(num1);
 }
+    
 multiply();
+//local variable
+/*
+var num = 20;
+function multiply(){
+    var num1 = 10;
+    console.log(num);
+    console.log(num);
+}
+multiply();
+*/
+//global variable
+/*
+var num = 20;
+function multiply(){
+    num = 10;
+    console.log(num);
+   
+}
+multiply();
+console.log(num);
+
+if (num2 > 10) {
+  var num2 = 200;
+}
+console.log(num2);
+
+let num = 20;
+if(num > 10){
+    let greet = "Hello";
+    console.log(greet);
+    great ="World";
+    console.log(greet);
+
+    let num =15;
+    console.log(num);
+}
+*/
